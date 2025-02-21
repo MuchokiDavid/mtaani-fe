d8430580466737e410de191dc7ad0340aeec5b95
david muchoki <dmmuchoki7@gmail.com>
2025-02-21 17:30:53 +0300

    Add Footer component for site-wide copyright notice

---
+++ src/components/pages/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <footer className="bg-gray-800 text-white py-8">
+                <div className="max-w-6xl mx-auto px-4 text-center">
+                    <p>&copy; 2023 PropertyPro. All rights reserved.</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
