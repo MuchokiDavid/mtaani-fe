97ec4d3f6e8d2dc0793a8d3503aa185060edaeb1
david muchoki <dmmuchoki7@gmail.com>
2025-02-21 17:31:33 +0300

    Add routing support with React Router and implement Login, Register, Home, About, Policy, and Terms components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/authentication/Login'
+import Register from './components/authentication/Register'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Policy from './components/pages/Policy'
+import Terms from './components/pages/Terms'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   // 
 
   return (
     <>
-    <p className='text-2xl'>Welcome</p>
+   
+      <Routes>
+          <Route path="/" element={ <Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/policy" element={<Policy/>} />
+          <Route path="/terms" element={<Terms/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+      </Routes>
     </>
   )
 }
