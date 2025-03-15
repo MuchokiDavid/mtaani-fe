bd54af27fc73580d4ccfeb6d584a07387233ad56
david muchoki <dmmuchoki7@gmail.com>
2025-03-15 22:07:02 +0300

    Refactor database functions for improved readability and consistency

--- src/components/database/db.js
+++ src/components/database/db.js
@@ -53,62 +53,145 @@ const initDB = async () => {
         },
     });
 };
+// const initDB = async () => {
+//   return openDB(DB_NAME, DB_VERSION, {
+//     upgrade(db) {
+//       // Create object stores if they don't exist
+//       if (!db.objectStoreNames.contains(STORE_MAINTENANCE)) {
+//         const maintenanceStore = db.createObjectStore(STORE_MAINTENANCE, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//         // Create an index on the 'roomId' field to establish a relationship with rooms
+//         maintenanceStore.createIndex("roomId", "roomId", { unique: false });
+//       }
+//       if (!db.objectStoreNames.contains(STORE_PROPERTIES)) {
+//         const propertiesStore = db.createObjectStore(STORE_PROPERTIES, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//         // Create an index on the 'landlordId' field to establish a relationship with users (landlords)
+//         propertiesStore.createIndex("landlordId", "landlordId", {
+//           unique: false,
+//         });
+//       }
+//       if (!db.objectStoreNames.contains(STORE_ROOMS)) {
+//         const roomsStore = db.createObjectStore(STORE_ROOMS, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//         // Create an index on the 'propertyId' field to establish a relationship with properties
+//         roomsStore.createIndex("propertyId", "propertyId", { unique: false });
+//       }
+//       if (!db.objectStoreNames.contains(STORE_USERS)) {
+//         const usersStore = db.createObjectStore(STORE_USERS, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//         // Create an index on the 'email' field
+//         usersStore.createIndex("email", "email", { unique: true });
+//         // Create an index on the 'roomId' field to establish a relationship with rooms (for tenants)
+//         usersStore.createIndex("roomId", "roomId", { unique: true }); // Ensure one tenant per room
+//       } else {
+//         // If the 'users' object store already exists, ensure the 'email' and 'roomId' indices exist
+//         const tx = db.transaction(STORE_USERS, "readwrite");
+//         const store = tx.objectStore(STORE_USERS);
+//         if (!store.indexNames.contains("email")) {
+//           store.createIndex("email", "email", { unique: true });
+//         }
+//         if (!store.indexNames.contains("roomId")) {
+//           store.createIndex("roomId", "roomId", { unique: true }); // Ensure one tenant per room
+//         }
+//         tx.done;
+//       }
+//       if (!db.objectStoreNames.contains(STORE_BOOKINGS)) {
+//         db.createObjectStore(STORE_BOOKINGS, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//       }
+//       if (!db.objectStoreNames.contains(STORE_ANNOUNCEMENTS)) {
+//         const announcementsStore = db.createObjectStore(STORE_ANNOUNCEMENTS, {
+//           keyPath: "id",
+//           autoIncrement: true,
+//         });
+//         // Create an index on the 'landlordId' field to establish a relationship with users (landlords)
+//         announcementsStore.createIndex("landlordId", "landlordId", {
+//           unique: false,
+//         });
+//       }
+//     },
+//   });
+// };
 
 // Function to add data to an object store
 const addData = async (storeName, data) => {
-    const db = await initDB();
-    const tx = db.transaction(storeName, "readwrite");
-    const store = tx.objectStore(storeName);
-    await store.add(data);
-    await tx.done;
+  const db = await initDB();
+  const tx = db.transaction(storeName, "readwrite");
+  const store = tx.objectStore(storeName);
+  await store.add(data);
+  await tx.done;
 };
 
 // Function to get all data from an object store
 const getAllData = async (storeName) => {
-    const db = await initDB();
-    const tx = db.transaction(storeName, "readonly");
-    const store = tx.objectStore(storeName);
-    return store.getAll();
+  const db = await initDB();
+  const tx = db.transaction(storeName, "readonly");
+  const store = tx.objectStore(storeName);
+  return store.getAll();
 };
 
 // Function to update data in an object store
 const updateData = async (storeName, id, data) => {
-    const db = await initDB();
-    const tx = db.transaction(storeName, "readwrite");
-    const store = tx.objectStore(storeName);
-    await store.put({ ...data, id });
-    await tx.done;
+  const db = await initDB();
+  const tx = db.transaction(storeName, "readwrite");
+  const store = tx.objectStore(storeName);
+  await store.put({ ...data, id });
+  await tx.done;
 };
 
 // Function to get data by ID from an object store
 const getDataById = async (storeName, id) => {
-    const db = await initDB();
-    const tx = db.transaction(storeName, "readonly");
-    const store = tx.objectStore(storeName);
-    return store.get(id);
+  const db = await initDB();
+  const tx = db.transaction(storeName, "readonly");
+  const store = tx.objectStore(storeName);
+  return store.get(id);
 };
 
 // Function to delete data from an object store
 const deleteData = async (storeName, id) => {
-    const db = await initDB();
-    const tx = db.transaction(storeName, "readwrite");
-    const store = tx.objectStore(storeName);
-    await store.delete(id);
-    await tx.done;
+  const db = await initDB();
+  const tx = db.transaction(storeName, "readwrite");
+  const store = tx.objectStore(storeName);
+  await store.delete(id);
+  await tx.done;
 };
 
 // get user by email
 const getUserByEmail = async (email) => {
-    try {
-        const db = await initDB();
-        const tx = db.transaction(STORE_USERS, "readonly");
-        const store = tx.objectStore(STORE_USERS);
-        const index = store.index("email");
-        return await index.get(email);
-    } catch (error) {
-        console.error("Error fetching user by email:", error);
-        throw error;
-    }
+  try {
+    const db = await initDB();
+    const tx = db.transaction(STORE_USERS, "readonly");
+    const store = tx.objectStore(STORE_USERS);
+    const index = store.index("email");
+    return await index.get(email);
+  } catch (error) {
+    console.error("Error fetching user by email:", error);
+    throw error;
+  }
 };
 
-export { addData, getAllData, updateData, deleteData, getDataById, getUserByEmail, STORE_MAINTENANCE, STORE_PROPERTIES, STORE_ROOMS, STORE_USERS, STORE_BOOKINGS, STORE_ANNOUNCEMENTS };
+export {
+  addData,
+  getAllData,
+  updateData,
+  deleteData,
+  getDataById,
+  getUserByEmail,
+  STORE_MAINTENANCE,
+  STORE_PROPERTIES,
+  STORE_ROOMS,
+  STORE_USERS,
+  STORE_BOOKINGS,
+  STORE_ANNOUNCEMENTS,
+};
